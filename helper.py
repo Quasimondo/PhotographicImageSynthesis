eda09c9f7f34009b3396b884846501aef04142c4
Mario Klingemann <Quasimondo@users.noreply.github.com>
2017-08-07 15:48:56 +0200

    Update helper.py
    Added nearest neighbor based semantic map method. 
    Replaced hardcoded "cityscapes"

--- helper.py
+++ helper.py
@@ -1,6 +1,8 @@
 import os,numpy as np
 from os.path import dirname, exists, join, splitext
 import json,scipy
+from sklearn.neighbors import NearestNeighbors
+
 class Dataset(object):
     def __init__(self, dataset_name):
         self.work_dir = dirname(os.path.realpath('__file__'))
@@ -8,18 +10,30 @@ class Dataset(object):
         with open(info_path, 'r') as fp:
             info = json.load(fp)
         self.palette = np.array(info['palette'], dtype=np.uint8)
+        self.nns = NearestNeighbors(1,metric='euclidean')
+        self.nns.fit(self.palette)  
 
 
-def get_semantic_map(path):
-    dataset=Dataset('cityscapes')
+def get_semantic_map(path,d ataset_name):
+    dataset=Dataset(dataset_name)
     semantic=scipy.misc.imread(path)
     tmp=np.zeros((semantic.shape[0],semantic.shape[1],dataset.palette.shape[0]),dtype=np.float32)
     for k in range(dataset.palette.shape[0]):
         tmp[:,:,k]=np.float32((semantic[:,:,0]==dataset.palette[k,0])&(semantic[:,:,1]==dataset.palette[k,1])&(semantic[:,:,2]==dataset.palette[k,2]))
     return tmp.reshape((1,)+tmp.shape)
 
-def print_semantic_map(semantic,path):
-    dataset=Dataset('cityscapes')
+def get_semantic_map_nn(path,dataset_name,ignoreIndex=-1):
+    dataset=Dataset(dataset_name)
+    semantic=scipy.misc.imread(path)
+    nns = dataset.nns.kneighbors(semantic.reshape(semantic.shape[0]*semantic.shape[1],3), 1, return_distance=False)
+    hot = np.identity(dataset.palette.shape[0])
+    if ignoreIndex > -1:
+        hot[ignoreIndex] *= 0.0
+    tmp = hot[nns]
+    return tmp.reshape((1,semantic.shape[0],semantic.shape[1],dataset.palette.shape[0]))
+
+def print_semantic_map(semantic,path,dataset_name):
+    dataset=Dataset(dataset_name)
     semantic=semantic.transpose([2,3,1,0])
     prediction=np.argmax(semantic,axis=2)
     color_image=dataset.palette[prediction.ravel()].reshape((prediction.shape[0],prediction.shape[1],3))
